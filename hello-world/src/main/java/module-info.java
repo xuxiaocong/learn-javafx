5bc14e0141fbe47e5c42b8101b277fdb97ac06ca
许晓聪 <1205049723@qq.com>
2020-08-04 00:33:23 +0800

    创建项目，并且上传第一个control

---
+++ hello-world/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.example {
+    requires javafx.controls;
+    exports org.example;
+    exports org.example.controls;
+}
---
+++ hello-world/src/main/java/org/example/controls/AccordionApp.java
@@ -0,0 +1,34 @@
+package org.example.controls;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
+
+/**
+ * 手风琴 Accordion demo
+ */
+public class AccordionApp extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        TitledPane t1 = new TitledPane("T1", new Button("Button1"));
+        TitledPane t2 = new TitledPane("T2", new Button("Button2"));
+        TitledPane t3 = new TitledPane("T3", new Button("Button3"));
+        Accordion accordion = new Accordion();
+        accordion.getPanes().addAll(t1, t2, t3);
+
+        var scene = new Scene(accordion, 640, 480);
+        stage.setScene(scene);
+        stage.setTitle("Accordion Control");
+        stage.show();
+
+    }
+}
