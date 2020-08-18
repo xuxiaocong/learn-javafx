31c11b861babb39004ecc8b9b7838bb84b7941bb
许晓聪 <1205049723@qq.com>
2020-08-18 21:28:27 +0800

    增加html加载方式

--- hello-html/src/main/java/org/example/App.java
+++ hello-html/src/main/java/org/example/App.java
@@ -21,12 +21,20 @@ public class App extends Application {
         WebView webView = new WebView();
         WebEngine engine = webView.getEngine();
 
-        // 获取本地html文件, 注意文件目录，注意与调用路径一致
+//        // 有三种加载html的方式(直接取消注释即可)
+//        // 1.加载在线站点
+//        engine.load("http://www.damengsanqian.com/");
+
+//        // 2.加载 HTML 字符串
+//        engine.loadContent("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Title</title></head><body><h1>这里是HTML页面String</h1></body></html>");
+
+        // 3.加载本地 HTML 文件, 注意文件目录，注意与调用路径一致
         String url = getClass().getResource("index.html").toExternalForm();
-        // 如果是网络上的站点，直接使用 engine.load("http://www.damengsanqian.com/")
         engine.load(url);
 
+
         var scene = new Scene(webView, 640, 480);
+        stage.setTitle("测试HTML");
         stage.setScene(scene);
         stage.show();
     }
